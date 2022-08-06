94f4a105987097733c5e30c5b6eaef2dfe99aa62
davidmospina <cuartomauri@gmail.com>
2022-08-06 19:55:04 +0200

    5 files modified, 8 added (tercer parte del tutorial terminada managind data)

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Product, products } from '../products';
-import { ActivatedRoute} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
+import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,12 +12,20 @@ import { ActivatedRoute} from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
 
   product: Product | undefined;
-  constructor(private route: ActivatedRoute) { }
-
+  constructor(private route: ActivatedRoute,
+    private cartService: CartService) { }
+  
+  
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
     this.product = products.find(paco => paco.id === productIdFromRoute)
   }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product,);
+    window.alert('Your product has been added to the cart!');
+  }
+
 
 }
